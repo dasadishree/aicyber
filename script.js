a033304a94ecae73eaa789b2d7607c7ea7cbf3df
dasadishree <dasadishree@gmail.com>
2026-02-18 20:25:47 -0500

    file raw text

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const upload = document.getElementById('imageUpload');
+const results = document.getElementById('results');
+
 upload.addEventListener('change', function() {
     const file = upload.files[0];
-    console.log('File selected:', file.name);
+    const reader= new FileReader();
+
+    reader.onload = function(e) {
+        const rawText = new TextDecoder('utf-8', {fatal: false}).decode(e.target.result);
+        console.log("Raw data loaded, length:", rawText.length);
+    };
+
+    reader.readAsArrayBuffer(file);
 });
